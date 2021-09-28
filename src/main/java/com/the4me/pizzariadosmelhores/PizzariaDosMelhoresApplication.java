8a4687337be25f43c4bed54959c021f616e74ee5
Weberson.dsn.erp <weberson.dsn.erp@alterdata.com.br>
2021-09-27 21:53:49 -0300

    Subir arquivos alterados

--- src/main/java/com/the4me/pizzariadosmelhores/PizzariaDosMelhoresApplication.java
+++ src/main/java/com/the4me/pizzariadosmelhores/PizzariaDosMelhoresApplication.java
@@ -2,8 +2,10 @@ package com.the4me.pizzariadosmelhores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PizzariaDosMelhoresApplication {
 
 	public static void main(String[] args) {
